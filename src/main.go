2599972e941d52de5e0ef98fddcc909c569b7517
Miles <milesasp@gmail.com>
2019-12-26 22:59:27 +0800

    use embedded

--- src/main.go
+++ src/main.go
@@ -13,33 +13,28 @@ func main() {
 	defer db.Close()
 	db.DropTable(&User{})
 
-	// db.SingularTable(true)
-
 	db.CreateTable(&User{})
 
-	for _, user := range users {
-		db.Create(&user)
+	for _, f := range db.NewScope(&User{}).Fields() {
+		println(f.Name)
 	}
 
-	db.Where(&User{Username: "tmacmillan"}).Delete(&User{})
 }
 
 type User struct {
-	UserID    int    `gorm:"primary_key"`
-	Username  string `sql:"type:VARCHAR(15);not null"`
-	FirstName string `sql:"size:100;not null" gorm:"column:FirstName"`
-	LastName  string `sql:"unique;unique_index;not null;DEFAULT:'Smith'" gorm:"column:LastName"`
-	Count     int    `gorm:"AUTO_INCREMENT"`
-	TempField bool
+	Model     gorm.Model `gorm:"embedded"`
+	FirstName string
+	LastName  string
 }
 
 func (u User) TableName() string {
 	return "stackeholders"
 }
 
-var users []User = []User{
-	User{Username: "adent", FirstName: "Arthur", LastName: "Dent"},
-	User{Username: "fprefect", FirstName: "Ford", LastName: "Prefect"},
-	User{Username: "tmacmillan", FirstName: "Tricia", LastName: "MacMillan"},
-	User{Username: "mrobot", FirstName: "Marvin", LastName: "Robot"},
-}
+//
+// var users []User = []User{
+// 	User{Username: "adent", FirstName: "Arthur", LastName: "Dent"},
+// 	User{Username: "fprefect", FirstName: "Ford", LastName: "Prefect"},
+// 	User{Username: "tmacmillan", FirstName: "Tricia", LastName: "MacMillan"},
+// 	User{Username: "mrobot", FirstName: "Marvin", LastName: "Robot"},
+// }
