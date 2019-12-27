b441bb3b68df997ea289c101624521575c155b8f
Miles <milesasp@gmail.com>
2019-12-27 21:38:21 +0800

    one to many releationship

--- src/main.go
+++ src/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq"
 )
@@ -15,14 +17,17 @@ func main() {
 	db.CreateTable(&User{})
 	db.DropTable(&Calendar{})
 	db.CreateTable(&Calendar{})
+	db.DropTable(&Appointment{})
+	db.CreateTable(&Appointment{})
 
-	db.Debug().Model(&Calendar{}).
-		AddForeignKey("user_id", "users(id)", "CASCADE", "CASCADE")
-
-	db.Save(&User{
+	db.Debug().Save(&User{
 		Username: "adent",
 		Calendar: Calendar{
 			Name: "Improbable Events",
+			Appointments: []Appointment{
+				{Subject: "Spontaneous Whale Generation"},
+				{Subject: "Saved from Vaccuum of Space"},
+			},
 		},
 	})
 
@@ -38,6 +43,16 @@ type User struct {
 
 type Calendar struct {
 	gorm.Model
-	Name   string
-	UserID uint
+	Name         string
+	UserID       uint
+	Appointments []Appointment
+}
+
+type Appointment struct {
+	gorm.Model
+	Subject     string
+	Description string
+	StartTime   time.Time
+	Length      uint
+	CalendarID  uint
 }
