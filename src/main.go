1a62ffc7adcc7a8e5836b17d68453cd8af5356af
Miles <milesasp@gmail.com>
2019-12-29 18:31:52 +0800

    drop table and create table

--- src/main.go
+++ src/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/jinzhu/gorm"
@@ -13,94 +12,11 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
-	// seedDB(db)
-	users := []User{}
-	// db.Find(&users)
-	db.Debug().Exec("SELECT * FROM users").Find(&users)
 
-	for i := range users {
-		fmt.Printf("\n%v\n", users[i])
-	}
-}
+	// appointment_user 是指多對多的那個 table
+	db.DropTableIfExists(&User{}, &Calendar{}, &Appointment{}, "appointment_user")
+	db.CreateTable(&User{}, &Calendar{}, &Appointment{}, &Attachment{})
 
-type UserViewModel struct {
-	FirstName    string
-	LastName     string
-	CalendarName string
-}
-
-func seedDB(db *gorm.DB) {
-	db.DropTable(&User{})
-	db.CreateTable(&User{})
-	db.DropTable(&Calendar{})
-	db.CreateTable(&Calendar{})
-	db.DropTable(&Appointment{})
-	db.CreateTable(&Appointment{})
-
-	users := map[string]*User{
-		"adent":       &User{Username: "adent", FirstName: "Arthur", LastName: "Dent"},
-		"fprefect":    &User{Username: "fprefect", FirstName: "Ford", LastName: "Prefect"},
-		"tmacmillan":  &User{Username: "tmacmillan", FirstName: "Tricia", LastName: "MacMillan"},
-		"zbeeblebrox": &User{Username: "zbeeblebox", FirstName: "Zaphod", LastName: "Beeblebrox"},
-		"mrobot":      &User{Username: "mrobot", FirstName: "Marvin", LastName: "Robot"},
-	}
-	for _, user := range users {
-		user.Calendar = Calendar{Name: "Calendar"}
-	}
-
-	users["adent"].AddAppointment(&Appointment{
-		Subject:   "Save House",
-		StartTime: parseTime("1979-07-02 08:00"),
-		Length:    60,
-	})
-	users["fprefect"].AddAppointment(&Appointment{
-		Subject:   "Get a Drink at Local Pub",
-		StartTime: parseTime("1979-07-02 10:00"),
-		Length:    11,
-		Attendees: []*User{users["adent"]},
-	})
-	users["fprefect"].AddAppointment(&Appointment{
-		Subject:   "Hitch a Ride",
-		StartTime: parseTime("1979-07-02 10:12"),
-		Length:    60,
-		Attendees: []*User{users["adent"]},
-	})
-	users["fprefect"].AddAppointment(&Appointment{
-		Subject:   "Attend Poetry Reading",
-		StartTime: parseTime("1979-07-02 11:00"),
-		Length:    30,
-		Attendees: []*User{users["adent"]},
-	})
-	users["fprefect"].AddAppointment(&Appointment{
-		Subject:   "Get Thrown into Space",
-		StartTime: parseTime("1979-07-02 11:40"),
-		Length:    5,
-		Attendees: []*User{users["adent"]},
-	})
-	users["fprefect"].AddAppointment(&Appointment{
-		Subject:   "Get Saved from Space",
-		StartTime: parseTime("1979-07-02 11:45"),
-		Length:    1,
-		Attendees: []*User{users["adent"]},
-	})
-	users["zbeeblebrox"].AddAppointment(&Appointment{
-		Subject:   "Explore Planet Builder's Homeworld",
-		StartTime: parseTime("1979-07-03 11:00"),
-		Length:    240,
-		Attendees: []*User{users["adent"], users["fprefect"], users["tmacmillan"],
-			users["mrobot"]},
-	})
-
-	for _, user := range users {
-		db.Save(&user)
-	}
-
-}
-
-func parseTime(timeRaw string) time.Time {
-	const timeLayout = "2006-01-02 15:04"
-	t, _ := time.Parse(timeLayout, timeRaw)
-	return t
 }
 
 type User struct {
@@ -109,10 +25,6 @@ type User struct {
 	FirstName string
 	LastName  string
 	Calendar  Calendar
-}
-
-func (u *User) AddAppointment(appt *Appointment) {
-	u.Calendar.Appointments = append(u.Calendar.Appointments, appt)
 }
 
 type Calendar struct {
@@ -124,10 +36,19 @@ type Calendar struct {
 
 type Appointment struct {
 	gorm.Model
-	Subject     string
-	Description string
-	StartTime   time.Time
-	Length      uint
-	CalendarID  uint
-	Attendees   []*User `gorm:"many2many:appointment_user"`
+	Subject           string
+	Description       string
+	StartTime         time.Time
+	Length            uint
+	CalendarID        uint
+	Recurring         bool
+	RecurrencePattern string
+	Attendees         []*User `gorm:"many2many:appointment_user"`
+	Attachments       []Attachment
+}
+
+type Attachment struct {
+	gorm.Model
+	Data          []byte
+	AppointmentID uint
 }
